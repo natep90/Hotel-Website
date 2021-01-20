265624539ef6141990a3f18a484ab5c7a5a528a8
Matt Bagnall <matt.bagnall33@gmail.com>
2021-01-20 16:44:42 +0000

    done

--- config.js
+++ config.js
@@ -12,10 +12,11 @@ var config = {
 		user: 'cew19wcu', // env var: PGUSER  - YOUR UEA username
 		database: 'cew19wcu', // env var: PGDATABASE  - YOUR UEA username
 		password: '', // env var: PGPASSWORD  - YOUR UEA password
-		host: 'cmpstudb-01.cmp.uea.ac.uk', // Server hosting the postgres database
+		host: 'localhost', // Server hosting the postgres database
 		port: 5432, // env var: PGPORT
 		max: 10, // max number of clients in the pool
-		idleTimeoutMillis: 30000 // how long a client is allowed to remain idle before being closed
+		idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+		use_env_variable: "DATABASE_URL"
 	},
 
 };
